55f86af6a70b31a56abf8b9b2ee2c6b83ea7f77c
= <kingliin814@gmail.com>
2016-02-14 13:15:12 +0800

    set admin_required to controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 	  before_action :find_product, only: [:show, :edit, :update, :destroy]
+	  before_action :authenticate_user!
+	  before_action :admin_required
 
 	  def index
 	    @products = Product.all
