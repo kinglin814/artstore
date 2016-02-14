153e36058f9c690bb9a3c6a25cf869f2233ff123
= <kingliin814@gmail.com>
2016-02-14 13:36:58 +0800

    generate products controller and set root

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  resources :products
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
