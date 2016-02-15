8888f4d240f634a77456753b3b1dc85a25fe53a3
= <kingliin814@gmail.com>
2016-02-15 14:31:51 +0800

    generate order token for params[:id], code show order pages

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,19 @@ class Order < ActiveRecord::Base
 	has_many :items, class_name: "OrderItem", dependent: :destroy
 	accepts_nested_attributes_for :info
 
+	before_create :generate_token
+
+	def generate_token
+		self.token = SecureRandom.uuid
+	end
+
+
 	def build_item_from_cart(cart)
 		cart.items.each do |product|
 			item = items.build #order_item.build
 			item.product_name = product.title
 			item.quantity = product.quantity
-			iten.price = product.price
+			item.price = product.price
 			item.save
 		end
 	end
