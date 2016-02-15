634d17d2077797312b6b07f1e5f1c0af50e50da1
= <kingliin814@gmail.com>
2016-02-15 14:22:01 +0800

    order's create action and set validates to order info

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,19 @@ class Order < ActiveRecord::Base
 	has_one :info, class_name: "OrderInfo", dependent: :destroy
 	has_many :items, class_name: "OrderItem", dependent: :destroy
 	accepts_nested_attributes_for :info
+
+	def build_item_from_cart(cart)
+		cart.items.each do |product|
+			item = items.build #order_item.build
+			item.product_name = product.title
+			item.quantity = product.quantity
+			iten.price = product.price
+			item.save
+		end
+	end
+
+	def calculate_total!(cart)
+		self.total = cart.total_price
+		self.save
+	end
 end
