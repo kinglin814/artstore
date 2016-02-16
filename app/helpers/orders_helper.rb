9ceff79ccf10e9d77a6e01ef7218f5e779a27527
= <kingliin814@gmail.com>
2016-02-16 13:07:23 +0800

    show order's state in admin pages

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,15 @@
 module OrdersHelper
+
+	def render_order_state(order)
+		t("orders.order_state.#{order.aasm_state}")
+	end
+
+	def render_paid_or_not(order)
+		if order.is_paid?
+			"已付款"
+		else
+			"未付款"
+		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   namespace :admin do
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :products
     resources :users do
       member do
